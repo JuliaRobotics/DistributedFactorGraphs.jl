be719faea386b19f3f58e3ca57b50baa8b312d6e
Johannes Terblanche <terblanche.j@gmail.com>
2019-08-07 16:12:33 +0200

    Don't run end-end on travis (Testing "TRAVIS" env variable)

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,8 +12,12 @@ for api in apis
     end
 end
 
-@testset "HexagonalLightGraphs" begin
-    include("HexagonalLightGraphs.jl")
+if !(get(ENV, "TRAVIS", "") == "true")
+    @testset "Local Testsets" begin
+        @testset "HexagonalLightGraphs" begin
+            include("HexagonalLightGraphs.jl")
+        end
+    end
 end
 
 # Test other interfaces that are not yet compatible for the general tests.
