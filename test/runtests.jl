022639750b7bb004bae8d701b0ef45cf1aa48293
gearsad <sclaassens.ad@gmail.com>
2019-10-06 18:03:16 -0500

    Removing MetaGraphsDFG and SymbolDFG exports, adding test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,24 @@ using GraphPlot # For plotting tests
 using DistributedFactorGraphs
 
 # Test each interface
-apis = [GraphsDFG, MetaGraphsDFG, SymbolDFG, LightDFG]
+# Still test LightDFG and MetaGraphsDFG for the moment until we remove in 0.4.2
+apis = [
+    GraphsDFG,
+    DistributedFactorGraphs.MetaGraphsDFG,
+    DistributedFactorGraphs.SymbolDFG,
+    LightDFG]
 for api in apis
     @testset "Testing Driver: $(api)" begin
         @info "Testing Driver: $(api)"
         global testDFGAPI = api
         include("interfaceTests.jl")
     end
+end
+
+# Test that we don't export LightDFG and MetaGraphsDFG
+@testset "Deprecated Drivers Test" begin
+    @test_throws UndefVarError SymbolDFG{NoSolverParams}()
+    @test_throws UndefVarError MetaGraphsDFG{NoSolverParams}()
 end
 
 # Test special cases
