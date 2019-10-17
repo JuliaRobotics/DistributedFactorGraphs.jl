3956990c57101136b6efb2d94e416dc17786a01a
gearsad <sclaassens.ad@gmail.com>
2019-10-17 09:01:51 -0500

    Fixing equality operators

--- src/services/DFGVariable.jl
+++ src/services/DFGVariable.jl
@@ -83,21 +83,20 @@ function unpack(dfg::G, d::PackedVariableNodeData)::VariableNodeData where G <: 
 end
 
 function compare(a::VariableNodeData, b::VariableNodeData)
-    TP = true
-    TP = TP && a.val == b.val
-    TP = TP && a.bw == b.bw
-    TP = TP && a.BayesNetOutVertIDs == b.BayesNetOutVertIDs
-    TP = TP && a.dimIDs == b.dimIDs
-    TP = TP && a.dims == b.dims
-    TP = TP && a.eliminated == b.eliminated
-    TP = TP && a.BayesNetVertID == b.BayesNetVertID
-    TP = TP && a.separator == b.separator
-    TP = TP && a.initialized == b.initialized
-    TP = TP && abs(a.inferdim - b.inferdim) < 1e-14
-    TP = TP && a.ismargin == b.ismargin
-    TP = TP && a.dontmargin == b.dontmargin
-    TP = TP && typeof(a.softtype) == typeof(b.softtype) # Can't do equals, need to check type.
-    return TP
+    a.val != b.val && @debug("val is not equal")==nothing && return false
+    a.bw != b.bw && @debug("bw is not equal")==nothing && return false
+    a.BayesNetOutVertIDs != b.BayesNetOutVertIDs && @debug("BayesNetOutVertIDs is not equal")==nothing && return false
+    a.dimIDs != b.dimIDs && @debug("dimIDs is not equal")==nothing && return false
+    a.dims != b.dims && @debug("dims is not equal")==nothing && return false
+    a.eliminated != b.eliminated && @debug("eliminated is not equal")==nothing && return false
+    a.BayesNetVertID != b.BayesNetVertID && @debug("BayesNetVertID is not equal")==nothing && return false
+    a.separator != b.separator && @debug("separator is not equal")==nothing && return false
+    a.initialized != b.initialized && @debug("initialized is not equal")==nothing && return false
+    abs(a.inferdim - b.inferdim) > 1e-14 && @debug("inferdim is not equal")==nothing && return false
+    a.ismargin != b.ismargin && @debug("ismargin is not equal")==nothing && return false
+    a.dontmargin != b.dontmargin && @debug("dontmargin is not equal")==nothing && return false
+    typeof(a.softtype) != typeof(b.softtype) && @debug("softtype is not equal")==nothing && return false
+    return true
 end
 
 """
@@ -113,10 +112,10 @@ end
 Equality check for VariableEstimate.
 """
 function ==(a::VariableEstimate, b::VariableEstimate)::Bool
-  a.solverKey != b.solverKey && return false
-  a.type != b.type && return false
-  a.estimate != b.estimate && return false
-  a.lastUpdatedTimestamp != b.lastUpdatedTimestamp && return false
+  a.solverKey != b.solverKey && @debug("solverKey are not equal")==nothing && return false
+  a.type != b.type && @debug("type is not equal")==nothing && return false
+  a.estimate != b.estimate && @debug("estimate are not equal")==nothing && return false
+  a.lastUpdatedTimestamp != b.lastUpdatedTimestamp && @debug("lastUpdatedTimestamp is not equal")==nothing && return false
   return true
 end
 
@@ -125,22 +124,22 @@ end
 Equality check for DFGVariable.
 """
 function ==(a::DFGVariable, b::DFGVariable)::Bool
-  a.label != b.label && return false
-  a.timestamp != b.timestamp && return false
-  a.tags != b.tags && return false
-  symdiff(keys(a.estimateDict), keys(b.estimateDict)) != Set(Symbol[]) && return false
+  a.label != b.label && @debug("label is not equal")==nothing && return false
+  a.timestamp != b.timestamp && @debug("timestamp is not equal")==nothing && return false
+  a.tags != b.tags && @debug("tags is not equal")==nothing && return false
+  symdiff(keys(a.estimateDict), keys(b.estimateDict)) != Set(Symbol[]) && @debug("estimateDict keys are not equal")==nothing && return false
   for k in keys(a.estimateDict)
-    a.estimateDict[k] != b.estimateDict[k] && return false
+    a.estimateDict[k] != b.estimateDict[k] && @debug("estimateDict[$k] is not equal")==nothing && return false
   end
-  symdiff(keys(a.solverDataDict), keys(b.solverDataDict)) != [] && return false
+  symdiff(keys(a.solverDataDict), keys(b.solverDataDict)) != Set(Symbol[]) && @debug("solverDataDict keys are not equal")==nothing && return false
   for k in keys(a.solverDataDict)
-    a.solverDataDict[k] != b.solverDataDict[k] && return false
+    a.solverDataDict[k] != b.solverDataDict[k] && @debug("solverDataDict[$k] is not equal")==nothing && return false
   end
-  a.smallData != b.smallData && return false
-  a.bigData != b.bigData && return false
-  a.ready != b.ready && return false
-  a.backendset != b.backendset && return false
-  a._internalId != b._internalId && return false
+  a.smallData != b.smallData && @debug("smallData is not equal")==nothing && return false
+  a.bigData != b.bigData && @debug("bigData is not equal")==nothing && return false
+  a.ready != b.ready && @debug("ready is not equal")==nothing && return false
+  a.backendset != b.backendset && @debug("backendset is not equal")==nothing && return false
+  a._internalId != b._internalId && @debug("_internalId is not equal")==nothing && return false
   return true
 end
 
