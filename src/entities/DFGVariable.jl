d666fdb4964ed3c11d5f2b466faa6562e25f41f6
gearsad <sclaassens.ad@gmail.com>
2019-11-17 14:43:07 -0600

    Additional variable in MeanMaxPPE

--- src/entities/DFGVariable.jl
+++ src/entities/DFGVariable.jl
@@ -72,11 +72,12 @@ Data container to store Parameteric Point Estimate (PPE) for mean and max.
 """
 struct MeanMaxPPE <: AbstractPointParametricEst
     solverKey::Symbol #repeated because of Sam's request
+	suggested::Vector{Float64}
     max::Vector{Float64}
     mean::Vector{Float64}
     lastUpdatedTimestamp::DateTime
 end
-MeanMaxPPE(solverKey::Symbol,max::Vector{Float64},mean::Vector{Float64}) = MeanMaxPPE(solverKey, max, mean, now())
+MeanMaxPPE(solverKey::Symbol, suggested::Vector{Float64}, max::Vector{Float64},mean::Vector{Float64}) = MeanMaxPPE(solverKey, suggested, max, mean, now())
 
 getMaxPPE(est::AbstractPointParametricEst) = est.max
 getMeanPPE(est::AbstractPointParametricEst) = est.mean
