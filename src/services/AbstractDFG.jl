c8da0a6bca72dfff5fb2a3e8559f5563d57fa800
dehann <dehann@users.noreply.github.com>
2019-11-23 07:05:33 -0500

    isSolveInProgress

--- src/services/AbstractDFG.jl
+++ src/services/AbstractDFG.jl
@@ -342,6 +342,20 @@ isSolveInProgress
 isSolvable(var::Union{DFGVariable, DFGFactor}) = var.ready
 
 """
+    $SIGNATURES
+
+Which variables or factors are currently being used by an active solver.  Useful for ensuring atomic transactions.
+
+DevNotes:
+- Will be renamed to `data.solveinprogress` which will be in VND, not DFGNode -- see DFG #201
+
+Related
+
+isSolvable
+"""
+isSolveInProgress(var::Union{DFGVariable, DFGFactor}; solveKey::Symbol=:default) = var.backendset
+
+"""
     $(SIGNATURES)
 Gets an empty and unique CloudGraphsDFG derived from an existing DFG.
 """
