1cb656f0190ae2792ea600efde7c33fe4d352e8f
Johannes Terblanche <terblanche.j@gmail.com>
2019-11-25 11:25:13 +0200

    remove LigthGraphs from DFG.jl to avoid conflics in IIF

--- src/services/AbstractDFG.jl
+++ src/services/AbstractDFG.jl
@@ -498,7 +498,7 @@ end
 Get an adjacency matrix for the DFG, returned as a tuple: adjmat::SparseMatrixCSC{Int}, var_labels::Vector{Symbol) fac_labels::Vector{Symbol).
 Rows are the factors, columns are the variables, with the corresponding labels in fac_labels,var_labels.
 """
-function getAdjacencyMatrixSparse(dfg::G; solvable::Int=0)::Tuple{LightGraphs.SparseMatrixCSC, Vector{Symbol}, Vector{Symbol}} where G <: AbstractDFG
+function getAdjacencyMatrixSparse(dfg::G; solvable::Int=0)::Tuple{SparseMatrixCSC, Vector{Symbol}, Vector{Symbol}} where G <: AbstractDFG
 	varLabels = map(v->v.label, getVariables(dfg, solvable=solvable))
 	factLabels = map(f->f.label, getFactors(dfg, solvable=solvable))
 
