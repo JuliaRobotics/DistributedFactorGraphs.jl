991646bfa781bd9b8d053bf323cd26a2716847a9
dehann <dehann.fourie@gmail.com>
2019-11-29 21:24:36 -0500

    add const

--- src/entities/DFGVariable.jl
+++ src/entities/DFGVariable.jl
@@ -237,7 +237,7 @@ SkeletonDFGFactor(label::Symbol, variableOrderSymbols::Vector{Symbol} = Symbol[]
 label(f::SkeletonDFGFactor) = f.label
 tags(f::SkeletonDFGFactor) = f.tags
 
-VariableDataLevel1 = Union{DFGVariable, DFGFactorSummary}
+const VariableDataLevel1 = Union{DFGVariable, DFGFactorSummary}
 
 """
     $SIGNATURES
