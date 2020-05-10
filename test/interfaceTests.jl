4044fd896eee77aa00408a4de517837a2de21520
Johannes Terblanche <terblanche.j@gmail.com>
2020-05-10 18:29:50 +0200

    some tests

--- test/interfaceTests.jl
+++ test/interfaceTests.jl
@@ -22,6 +22,41 @@ end
     global fac0, fac1, fac2 = DFGFactorSCA()
 end
 
+
+@testset "Custom Printing" begin
+
+iobuf = IOBuffer()
+# for now just test the interface and a bit of output
+@test printVariable(var1) == nothing
+@test printFactor(fac1) == nothing
+
+@test printVariable(iobuf, var1, skipfields=[:timestamp, :solver, :ppe]) == nothing
+@test String(take!(iobuf)) == "DFGVariable{TestSofttype1}\nlabel:\n:a\ntags:\nSet([:VARIABLE, :POSE])\nsmallData:\nDict(\"small\"=>\"data\")\nbigData:\nDict{Symbol,AbstractBigDataEntry}()\n_dfgNodeParams:\nDFGNodeParams(0)\n"
+
+@test printVariable(iobuf, var1, short=true) == nothing
+@test String(take!(iobuf)) == "DFGVariable{TestSofttype1}\nlabel: a\ntags: Set([:VARIABLE, :POSE])\nsize marginal samples: (1, 1)\nkde bandwidths: [0.0]\nNo PPEs\n"
+
+
+@test printFactor(iobuf, fac1, skipfields=[:timestamp, :solver]) == nothing
+@test occursin(r"DFGFactor.*\nlabel:\n:abf1", String(take!(iobuf)))
+
+String(take!(iobuf)) == "DFGFactor{TestCCW{TestFunctorInferenceType1}}\nlabel:\n:abf1\ntags:\nSet([:tag1, :tag2])\nsolvable:\n0\n_dfgNodeParams:\nDFGNodeParams(1)\n_variableOrderSymbols:\n[:a, :b]\n"
+
+@test printFactor(iobuf, fac1, short=true) == nothing
+@test occursin(r"DFGFactor.*\nlabel.*\ntimestamp.*\ntags.*\nsolvable", String(take!(iobuf)))
+
+# s = String(take!(iobuf))
+
+@test show(var1) == nothing
+@test show(fac1) == nothing
+
+@test show(iobuf, MIME("text/plain"), var1) == nothing
+isapprox(length(take!(iobuf)), 452, atol=10)
+@test show(iobuf, MIME("text/plain"), fac1) == nothing
+isapprox(length(take!(iobuf)), 301, atol=10)
+
+end
+
 @testset "Variables and Factors CRUD and SET" begin
     VariablesandFactorsCRUD_SET!(fg1, var1, var2, var3, fac0, fac1, fac2)
 end
