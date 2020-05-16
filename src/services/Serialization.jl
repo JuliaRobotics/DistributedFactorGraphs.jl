5efa24dac62541b6e946b932c8657bb96fdc2177
Johannes Terblanche <terblanche.j@gmail.com>
2020-05-16 12:53:12 +0200

    Remove get/setSerializationModule and always use Main close #462

--- src/services/Serialization.jl
+++ src/services/Serialization.jl
@@ -177,41 +177,13 @@ end
 ##==============================================================================
 
 """
-    $(SIGNATURES)
-
-Deserialization of IncrementalInference objects require discovery of foreign types.
-
-Example:
-
-Template to tunnel types from a user module:
-```julia
-# or more generic solution -- will always try Main if available
-IIF.setSerializationNamespace!(Main)
-
-# or a specific package such as RoME if you import all variable and factor types into a specific module.
-using RoME
-IIF.setSerializationNamespace!(RoME)
-```
-"""
-function setSerializationModule!(dfg::G, mod::Module)::Nothing where G <: AbstractDFG
-    @warn "Setting serialization module from AbstractDFG - override this in the '$(typeof(dfg)) structure! This is being ignored."
-end
-
-function getSerializationModule(dfg::G)::Module where G <: AbstractDFG
-    @warn "Retrieving serialization module from AbstractDFG - override this in the '$(typeof(dfg)) structure! This is returning Main"
-    return Main
-end
-
-"""
   $(SIGNATURES)
 Get a type from the serialization module inside DFG.
 """
 function getTypeFromSerializationModule(dfg::G, moduleType::Symbol) where G <: AbstractDFG
     st = nothing
-    mainmod = getSerializationModule(dfg)
-    mainmod == nothing && error("Serialization module is null - please call setSerializationNamespace!(\"Main\" => Main) in your main program.")
     try
-        st = getfield(mainmod, Symbol(moduleType))
+        st = getfield(Main, Symbol(moduleType))
     catch ex
         @error "Unable to deserialize soft type $(d.softtype)"
         io = IOBuffer()
