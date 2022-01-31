13bbbfd948b6d2acfc4c4cc211fcaacc228f47f3
Johannes Terblanche <Affie@users.noreply.github.com>
2022-01-31 22:37:22 +0200

    Escalate inferdim depwarn to error

--- src/Deprecated.jl
+++ src/Deprecated.jl
@@ -10,75 +10,56 @@ function Base.convert(::Type{DateTime}, ts::ZonedDateTime)
 end
 
 ## ================================================================================
-## Deprecate before v0.17
+## Deprecate before v0.19 - Kept longer with error
 ##=================================================================================
-
-
-Base.propertynames(x::VariableNodeData, private::Bool=false) = private ? (:inferdim, :infoPerCoord) : (:infoPerCoord,)
 
 Base.getproperty(x::VariableNodeData,f::Symbol) = begin
   if f == :inferdim
-    Base.depwarn("vnd.inferdim::Float64 is deprecated, use vnd.infoPerCoord::Vector{Float64} instead", :getproperty)
-    # @warn "vnd.inferdim is deprecated, use .infoPerCoord instead"
-    getfield(x, :infoPerCoord)
+    error("vnd.inferdim::Float64 was deprecated and is now obsolete, use vnd.infoPerCoord::Vector{Float64} instead")
   else
     getfield(x,f)
   end
 end
 
-function Base.setproperty!(x::VariableNodeData, f::Symbol, val::Real)
-  _val = if f == :inferdim
-    Base.depwarn("vnd.inferdim::Float64 is deprecated, use vnd.infoPerCoord::Vector{Float64} instead", :setproperty!)
-    f = :infoPerCoord
-    Float64[val;]
-  else
-    val
-  end
-  return setfield!(x, f, _val)
-end
-
-function Base.setproperty!(x::VariableNodeData, f::Symbol, val::AbstractVector{<:Real})
+function Base.setproperty!(x::VariableNodeData, f::Symbol, val)
   if f == :inferdim
-    Base.depwarn("vnd.inferdim::Float64 is deprecated, use vnd.infoPerCoord::Vector{Float64} instead", :setproperty!)
-    f = :infoPerCoord
+    error("vnd.inferdim::Float64 was deprecated and is now obsolete, use vnd.infoPerCoord::Vector{Float64} instead")
   end
   return setfield!(x, f, val)
 end
-
-#
-
-Base.propertynames(x::PackedVariableNodeData, private::Bool=false) = private ? (:inferdim, :infoPerCoord) : (:infoPerCoord,)
 
 Base.getproperty(x::PackedVariableNodeData,f::Symbol) = begin
   if f == :inferdim
-    Base.depwarn("pvnd.inferdim::Float64 is deprecated, use vnd.infoPerCoord::Vector{Float64} instead", :getproperty)
-    getfield(x, :infoPerCoord)
+    error("pvnd.inferdim::Float64 was deprecated and is now obsolete, use vnd.infoPerCoord::Vector{Float64} instead")
   else
     getfield(x,f)
   end
 end
 
-function Base.setproperty!(x::PackedVariableNodeData, f::Symbol, val::Real)
-  _val = if f == :inferdim
-    Base.depwarn("pvnd.inferdim::Float64 is deprecated, use vnd.infoPerCoord::Vector{Float64} instead", :setproperty!)
-    f = :infoPerCoord
-    Float64[val;]
-  else
-    val
-  end
-  return setfield!(x, f, _val)
-end
-
-function Base.setproperty!(x::PackedVariableNodeData, f::Symbol, val::AbstractVector{<:Real})
+function Base.setproperty!(x::PackedVariableNodeData, f::Symbol, val)
   if f == :inferdim
-    Base.depwarn("pvnd.inferdim::Float64 is deprecated, use vnd.infoPerCoord::Vector{Float64} instead", :setproperty!)
-    f = :infoPerCoord
+    error("pvnd.inferdim::Float64 was deprecated and is now obsolete, use vnd.infoPerCoord::Vector{Float64} instead")
   end
   return setfield!(x, f, val)
 end
 
 
-@deprecate VariableNodeData(val::Vector,bw::AbstractMatrix{<:Real},BayesNetOutVertIDs::AbstractVector{Symbol},dimIDs::AbstractVector{Int},dims::Int,eliminated::Bool,BayesNetVertID::Symbol,separator::AbstractVector{Symbol},variableType,initialized::Bool,inferdim::Real,w...;kw...) VariableNodeData(val,bw,BayesNetOutVertIDs,dimIDs,dims,eliminated,BayesNetVertID,separator,variableType,initialized,Float64[inferdim;],w...;kw...)
+
+function VariableNodeData(val::Vector,
+                          bw::AbstractMatrix{<:Real},
+                          BayesNetOutVertIDs::AbstractVector{Symbol},
+                          dimIDs::AbstractVector{Int},
+                          dims::Int,
+                          eliminated::Bool,
+                          BayesNetVertID::Symbol,
+                          separator::AbstractVector{Symbol},
+                          variableType,
+                          initialized::Bool,
+                          inferdim::Real,
+                          w...; kw...)
+  error("VariableNodeData field inferdim was deprecated and is now obsolete, use infoPerCoord instead")
+end
+
 
 
 ## ================================================================================
