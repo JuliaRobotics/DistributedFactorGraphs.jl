fc9dca4244e557953ebf0b0a5b6efbf7f7d6aa91
dehann <dehann.fourie@gmail.com>
2022-08-24 21:11:48 -0700

    typo fix

--- test/test_defVariable.jl
+++ test/test_defVariable.jl
@@ -24,7 +24,7 @@ struct NotAManifold end
 @test getDimension(TestVarType2) === 6
 
 @test getPointType(TestVarType1) == Vector{Float64}
-@test getPointType(TestVarType2) == ArrayPartition{FLoat64, Tuple{Vector{Float64}, Matrix{Float64}}}
+@test getPointType(TestVarType2) == ArrayPartition{Float64, Tuple{Vector{Float64}, Matrix{Float64}}}
 
 @test getPointIdentity(TestVarType1) == zeros(3)
 @test getPointIdentity(TestVarType2).x[1] == ArrayPartition(zeros(3), diagm(ones(3))).x[1]
