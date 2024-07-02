bc7a79e5f736a8bcbb27439b4926a2d5a1cbf64f
Johannes Terblanche <6612981+Affie@users.noreply.github.com>
2024-07-02 13:09:43 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Deprecated.jl
+++ src/Deprecated.jl
@@ -2,11 +2,11 @@
 ## ================================================================================
 ## Remove in v0.25
 ##=================================================================================
-@deprecate getBlobEntry(var::AbstractDFGVariable, key::AbstractString) = getBlobEntryFirst(var, Regex(key))
+@deprecate getBlobEntry(var::AbstractDFGVariable, key::AbstractString) =
+    getBlobEntryFirst(var, Regex(key))
 
 ## ================================================================================
 ## Remove in v0.24
 ##=================================================================================
 #NOTE free up getNeighbors to return the variables or factors
 @deprecate getNeighbors(args...; kwargs...) listNeighbors(args...; kwargs...)
-
