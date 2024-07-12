fc8923b1c57838f03502d675af7c9af585bd2e8e
Johannes Terblanche <6612981+Affie@users.noreply.github.com>
2024-07-12 17:59:49 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/services/DFGFactor.jl
+++ src/services/DFGFactor.jl
@@ -86,7 +86,8 @@ macro defFactorType(structname, factortype, manifold)
                 Z::T
             end
 
-            Base.@__doc__ struct $packedstructname{T<:PackedSamplableBelief} <: AbstractPackedFactor 
+            Base.@__doc__ struct $packedstructname{T <: PackedSamplableBelief} <:
+                                 AbstractPackedFactor
                 Z::T
             end
 
