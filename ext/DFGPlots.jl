24733240d7828b0481a64a0bb58faf8a82cf1b72
Johannes Terblanche <Affie@users.noreply.github.com>
2025-04-02 17:07:04 +0200

    Change GraphPlot -> GraphMakie

--- ext/DFGPlots.jl
+++ ext/DFGPlots.jl
@@ -2,13 +2,13 @@ module DFGPlots
 
 using Colors
 using Graphs
-using GraphPlot
 using DocStringExtensions
-import GraphPlot: gplot
+using GraphMakie
 
 using DistributedFactorGraphs
 
-import DistributedFactorGraphs.plotDFG
+import DistributedFactorGraphs: plotDFG
+import GraphMakie: graphplot
 
 export plotDFG, DFGPlotProps
 
@@ -17,41 +17,59 @@ struct DFGPlotProps
     nodesize::NamedTuple{(:var, :fac), Tuple{Float64, Float64}}
     shape::NamedTuple{(:var, :fac), Tuple{Symbol, Symbol}} #not upported yet
 
-    layout::Function #spring_layout, spectral_layout
+    layout #FIXME add layout type
     drawlabels::Bool
 end
 
 function DFGPlotProps()
     return DFGPlotProps(
         (var = colorant"seagreen", fac = colorant"cyan3"),
-        (var = 1.0, fac = 0.3),
+        (var = 30.0, fac = 10.0),
         (var = :box, fac = :elipse),
-        spring_layout,
+        GraphMakie.Stress(),
         true,
     )
 end
 
-function plotDFG(dfg::GraphsDFG, p::DFGPlotProps = DFGPlotProps())
+function plotDFG(dfg::GraphsDFG; p::DFGPlotProps = DFGPlotProps(), interactive::Bool = true)
     nodetypes = [haskey(dfg.g.variables, s) for s in dfg.g.labels]
 
     nodesize = [isVar ? p.nodesize.var : p.nodesize.fac for isVar in nodetypes]
     # nodelabel = [isVar ? string(get_prop(dfg.g,i,:label)) : "" for (i,isVar) in enumerate(nodetypes)]
     if p.drawlabels
-        nodelabel = [nodetypes[i] ? string(s) : "" for (i, s) in enumerate(dfg.g.labels)]
+        ilabels = [nodetypes[i] ? string(s) : "" for (i, s) in enumerate(dfg.g.labels)]
     else
-        nodelabel = nothing
+        ilabels = nothing
     end
 
     nodefillc = [isVar ? p.nodefillc.var : p.nodefillc.fac for isVar in nodetypes]
 
-    return gplot(
+    figaxpl = graphplot(
         dfg.g;
-        nodelabel = nodelabel,
-        nodesize = nodesize,
-        nodefillc = nodefillc,
+        ilabels,
         layout = p.layout,
+        node_size = nodesize,
+        # nodesize = nodesize,
+        # nodefillc = nodefillc,
+        # layout = p.layout,
     )
-end
+
+    (f, ax, p) = figaxpl
+    ax.aspect = GraphMakie.DataAspect()
+    if interactive
+        function node_drag_action(state, idx, event, axis)
+            p[:node_pos][][idx] = event.data
+            p[:node_pos][] = p[:node_pos][]
+        end
+        
+        GraphMakie.hidedecorations!(ax)
+        GraphMakie.hidespines!(ax)
+        ndrag = NodeDragHandler(node_drag_action)
+        GraphMakie.deregister_interaction!(ax, :rectanglezoom)
+        GraphMakie.register_interaction!(ax, :ndrag, ndrag)
+    end
+    return figaxpl
+end 
 
 function plotDFG(dfg::AbstractDFG, p::DFGPlotProps = DFGPlotProps())
     # TODO implement convert functions
@@ -60,8 +78,8 @@ function plotDFG(dfg::AbstractDFG, p::DFGPlotProps = DFGPlotProps())
     return plotDFG(ldfg, p)
 end
 
-function gplot(dfg::GraphsDFG; keyargs...)
-    return gplot(dfg.g; keyargs...)
+function graphplot(dfg::GraphsDFG; keyargs...)
+    return graphplot(dfg.g; keyargs...)
 end
 
 end
