989d5a213568c3dad8b0c0c0fee27180d283af43
dehann <dehann.fourie@gmail.com>
2025-04-25 07:20:23 -0700

    more docs for VariableDFG

--- src/entities/DFGVariable.jl
+++ src/entities/DFGVariable.jl
@@ -210,6 +210,11 @@ getEstimateFields(::MeanMaxPPE) = [:suggested, :max, :mean]
     $(TYPEDEF)
 
 The Variable information packed in a way that accomdates multi-lang using json.
+
+Notes:
+- timestamp is a `ZonedDateTime` in UTC.
+- nstime can be used as mission time, with the convention that the timestamp millis coincide with the mission start nstime
+  - e.g. timestamp is `2020-01-01 06:30:01.250 UTC` and first nstime is `250_000_000`.
 """
 Base.@kwdef struct VariableDFG <: AbstractDFGVariable
     id::Union{UUID, Nothing} = nothing
