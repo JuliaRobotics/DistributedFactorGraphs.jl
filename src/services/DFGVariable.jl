7f240e45e198f52d3fee097809776d80af26e408
Johannes Terblanche <6612981+Affie@users.noreply.github.com>
2025-07-16 10:36:08 +0200

    implement mergeStates! (#1145)

--- src/services/DFGVariable.jl
+++ src/services/DFGVariable.jl
@@ -733,6 +733,20 @@ function mergeState!(v::VariableCompute, vnd::State)
     return 1
 end
 
+function mergeStates!(dfg::AbstractDFG, varLabel_state_pairs::Vector{Pair{Symbol, State}})
+    cnt = asyncmap(varLabel_state_pairs) do (varLabel, state)
+        return mergeState!(dfg, varLabel, state)
+    end
+    return sum(cnt)
+end
+
+function mergeStates!(dfg::AbstractDFG, variableLabel::Symbol, states::Vector{<:State})
+    cnt = asyncmap(states) do state
+        return mergeState!(dfg, variableLabel, state)
+    end
+    return sum(cnt)
+end
+
 function copytoState!(
     dfg::AbstractDFG,
     variableLabel::Symbol,
