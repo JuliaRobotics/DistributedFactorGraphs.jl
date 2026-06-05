573c5c4e0333a6ff94aaf13e257526060ab14737
Johannes Terblanche <Affie@users.noreply.github.com>
2026-06-05 15:06:29 +0200

    Use test workspace for IncrementalInference sources

--- test/runtests.jl
+++ test/runtests.jl
@@ -76,47 +76,7 @@ DFG.@usingDFG true
         end
     end
 
-    if get(ENV, "IIF_TEST", "true") == "true"
-
-        # Switch to our upstream test branch.
-        #FIXME This is a temporary fix to use the develop branch of AMP.
-        # Pkg.add(PackageSpec(; name = "ApproxManifoldProducts", rev = "develop"))
-        #FIXME This is a temporary fix to use the develop branch of IIF.
-        # Pkg.add(PackageSpec(; name = "IncrementalInference", rev = "upstream/dfg_integration_test"))
-        # Pkg.add(PackageSpec(; name = "IncrementalInference", rev = "develop"))
-        if get(ENV, "IIF_TEST_DEV", "true") == "true"
-            Pkg.add(
-                PackageSpec(;
-                    url = "https://github.com/JuliaRobotics/IncrementalInference.jl.git",
-                    subdir = "IncrementalInferenceTypes",
-                    rev = "develop",
-                ),
-            )
-            Pkg.add(
-                PackageSpec(;
-                    url = "https://github.com/JuliaRobotics/IncrementalInference.jl.git",
-                    subdir = "IncrementalInference",
-                    rev = "develop",
-                ),
-            )
-        else
-            Pkg.develop(;
-                path = joinpath(
-                    DEPOT_PATH[1],
-                    "dev",
-                    "IncrementalInference",
-                    "IncrementalInferenceTypes",
-                ),
-            )
-            Pkg.develop(;
-                path = joinpath(
-                    DEPOT_PATH[1],
-                    "dev",
-                    "IncrementalInference",
-                    "IncrementalInference",
-                ),
-            )
-        end
+    if get(ENV, "IIF_TEST", "true") == "true"    
 
         @info "------------------------------------------------------------------------"
         @info "These tests are using IncrementalInference to do additional driver tests"
